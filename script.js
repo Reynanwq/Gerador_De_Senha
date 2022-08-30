a56673c670bbccb39ff51df15131daa31d5ac18d
Reynan Paiva <reynanwq@gmail.com>
2022-08-29 23:30:59 -0300

    pegando os ID

---
+++ script.js
@@ -0,0 +1,5 @@
+let slider = document.querySelector("#slider");
+let button = document.querySelector("#button");
+let sizepassword = document.querySelector("#valor");
+let password = document.querySelector("#password");
+let containerPassword = document.querySelector("#container-password");
